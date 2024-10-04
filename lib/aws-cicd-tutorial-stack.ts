7783b012f2eee36c0ead01bd38e085449c1639b1
Mohsin Hassan <mohsin@mhlab.me>
2024-10-04 12:03:10 +0400

    removing the resources

--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -10,85 +10,85 @@ export class AwsCicdTutorialStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Load the environment .env file.
-    dotenv.config();
+    // // Load the environment .env file.
+    // dotenv.config();
 
-    // Create a table to store some data.
-    const table = new dynamodb.Table(this, "VisitorTimeTable", {
-      partitionKey: {
-        name: "key",
-        type: dynamodb.AttributeType.STRING,
-      },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-    });
-
-    // Create a Lambda function using a Docker image
-    const lambdaFunction = new lambda.DockerImageFunction(this, "LambdaFunction", {
-      code: lambda.DockerImageCode.fromImageAsset("lambda-docker"), // Path to the Dockerfile
-      environment: {
-        VERSION: process.env.VERSION || "0.0",
-        TABLE_NAME: table.tableName,
-      },
-    });
-
-
-    const seleniumlambdaFunction = new lambda.DockerImageFunction(this, "seleniumLambdaFunction", {
-      code: lambda.DockerImageCode.fromImageAsset("selenium-docker"), // Path to the Dockerfile
-      memorySize: 1024,  // Increase memory to 1 GB (can be set up to 10,240 MB)
-      timeout: cdk.Duration.seconds(300),  // Increase timeout to 5 minutes (300 seconds)  Duration.minutes(5)
-      environment: {
-        VERSION: process.env.VERSION || "0.0",
-        TABLE_NAME: table.tableName,
-      },
-    });
-    // // Create EventBridge rule for daily schedule (runs at 00:00 UTC)
-    // const dailyRule = new events.Rule(this, 'DailyScheduleRule', {
-    //   schedule: events.Schedule.cron({ minute: '0', hour: '0' }), // Runs daily at 00:00 UTC
+    // // Create a table to store some data.
+    // const table = new dynamodb.Table(this, "VisitorTimeTable", {
+    //   partitionKey: {
+    //     name: "key",
+    //     type: dynamodb.AttributeType.STRING,
+    //   },
+    //   billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     // });
 
-    // // Set the Lambda function as the target for the EventBridge rule
-    // dailyRule.addTarget(new targets.LambdaFunction(lambdaFunction));
+    // // Create a Lambda function using a Docker image
+    // const lambdaFunction = new lambda.DockerImageFunction(this, "LambdaFunction", {
+    //   code: lambda.DockerImageCode.fromImageAsset("lambda-docker"), // Path to the Dockerfile
+    //   environment: {
+    //     VERSION: process.env.VERSION || "0.0",
+    //     TABLE_NAME: table.tableName,
+    //   },
+    // });
 
-    // Second Lambda Function (Newly added)
-    const secondLambdaFunction = new lambda.DockerImageFunction(this, "SecondLambdaFunction", {
-      code: lambda.DockerImageCode.fromImageAsset("lambda-docker2"),  // Path to second Dockerfile
-      environment: {
-        VERSION: process.env.VERSION || "0.0",
-      },
-    });
 
-    // Grant the Lambda function read/write access to the DynamoDB table
-    table.grantReadWriteData(lambdaFunction);
+    // const seleniumlambdaFunction = new lambda.DockerImageFunction(this, "seleniumLambdaFunction", {
+    //   code: lambda.DockerImageCode.fromImageAsset("selenium-docker"), // Path to the Dockerfile
+    //   memorySize: 1024,  // Increase memory to 1 GB (can be set up to 10,240 MB)
+    //   timeout: cdk.Duration.seconds(300),  // Increase timeout to 5 minutes (300 seconds)  Duration.minutes(5)
+    //   environment: {
+    //     VERSION: process.env.VERSION || "0.0",
+    //     TABLE_NAME: table.tableName,
+    //   },
+    // });
+    // // // Create EventBridge rule for daily schedule (runs at 00:00 UTC)
+    // // const dailyRule = new events.Rule(this, 'DailyScheduleRule', {
+    // //   schedule: events.Schedule.cron({ minute: '0', hour: '0' }), // Runs daily at 00:00 UTC
+    // // });
 
-    // Add a function URL for the Lambda function
-    const functionUrl = lambdaFunction.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-      cors: {
-        allowedOrigins: ["*"],
-        allowedMethods: [lambda.HttpMethod.ALL],
-        allowedHeaders: ["*"],
-      },
-    });
+    // // // Set the Lambda function as the target for the EventBridge rule
+    // // dailyRule.addTarget(new targets.LambdaFunction(lambdaFunction));
 
-    // Add a function URL for the Lambda function
-    const functionUrl2 = secondLambdaFunction.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-      cors: {
-        allowedOrigins: ["*"],
-        allowedMethods: [lambda.HttpMethod.ALL],
-        allowedHeaders: ["*"],
-      },
-    });
+    // // Second Lambda Function (Newly added)
+    // const secondLambdaFunction = new lambda.DockerImageFunction(this, "SecondLambdaFunction", {
+    //   code: lambda.DockerImageCode.fromImageAsset("lambda-docker2"),  // Path to second Dockerfile
+    //   environment: {
+    //     VERSION: process.env.VERSION || "0.0",
+    //   },
+    // });
 
-    // Output the function URL after deployment
-    new cdk.CfnOutput(this, "Url", {
-      value: functionUrl.url,
-    });
+    // // Grant the Lambda function read/write access to the DynamoDB table
+    // table.grantReadWriteData(lambdaFunction);
 
-    // Output the function URL after deployment
-    new cdk.CfnOutput(this, "Url2", {
-      value: functionUrl2.url,
-    });
+    // // Add a function URL for the Lambda function
+    // const functionUrl = lambdaFunction.addFunctionUrl({
+    //   authType: lambda.FunctionUrlAuthType.NONE,
+    //   cors: {
+    //     allowedOrigins: ["*"],
+    //     allowedMethods: [lambda.HttpMethod.ALL],
+    //     allowedHeaders: ["*"],
+    //   },
+    // });
+
+    // // Add a function URL for the Lambda function
+    // const functionUrl2 = secondLambdaFunction.addFunctionUrl({
+    //   authType: lambda.FunctionUrlAuthType.NONE,
+    //   cors: {
+    //     allowedOrigins: ["*"],
+    //     allowedMethods: [lambda.HttpMethod.ALL],
+    //     allowedHeaders: ["*"],
+    //   },
+    // });
+
+    // // Output the function URL after deployment
+    // new cdk.CfnOutput(this, "Url", {
+    //   value: functionUrl.url,
+    // });
+
+    // // Output the function URL after deployment
+    // new cdk.CfnOutput(this, "Url2", {
+    //   value: functionUrl2.url,
+    // });
 
   }
 }
